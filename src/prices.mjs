a7c8dc62ee5d4a7ca9252603d411fca05f60465b
Abidzar M. G. Kurniawan <abidzarisprivate@gmail.com>
2022-07-06 04:46:25 +0000

    Replace all usages of the Date class with the Temporal.PlainDate class

--- src/prices.mjs
+++ src/prices.mjs
@@ -25,7 +25,9 @@ function createApp(database) {
 
   function parseDate(dateString) {
     if (dateString) {
-      return new Date(dateString);
+      // return new Date(dateString);
+      // console.log(Temporal.PlainDate(dateString))
+      return Temporal.PlainDate.from(dateString);
     }
   }
 
@@ -76,18 +78,19 @@ function createApp(database) {
   }
 
   function isMonday(date) {
-    return date.getDay() === 1;
+    // console.log(date.dayOfWeek);
+    return date.dayOfWeek === 1;
   }
 
   function isHoliday(date) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
-      let holiday = new Date(row.holiday);
+      let holiday = Temporal.PlainDate.from(row.holiday);
       if (
         date &&
-        date.getFullYear() === holiday.getFullYear() &&
-        date.getMonth() === holiday.getMonth() &&
-        date.getDate() === holiday.getDate()
+        date.year === holiday.year &&
+        date.month === holiday.month &&
+        date.day === holiday.day
       ) {
         return true;
       }
